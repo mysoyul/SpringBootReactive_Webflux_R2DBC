ddcf8535179ee97806284beb18fc5a250336f2f1
mysoyul <vega2k_@naver.com>
2021-12-20 15:56:57 +0900

    service

--- src/main/java/myspringboot/reactive/r2dbc/dao/CustomerDao.java
+++ src/main/java/myspringboot/reactive/r2dbc/dao/CustomerDao.java
@@ -1,11 +1,13 @@
 package myspringboot.reactive.r2dbc.dao;
 
 import myspringboot.reactive.r2dbc.dto.Customer;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Component
 public class CustomerDao {
     private static void sleepExecution(int i){
         try {
