5422565da5c38bf4a94703afb19bf542f0f08d10
mysoyul <vega2k_@naver.com>
2021-12-20 15:51:57 +0900

    dto

---
+++ src/main/java/myspringboot/reactive/r2dbc/dto/Customer.java
@@ -0,0 +1,13 @@
+package myspringboot.reactive.r2dbc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private int id;
+    private String name;
+}
