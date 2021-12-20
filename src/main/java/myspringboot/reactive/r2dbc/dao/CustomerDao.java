641ffefca14fc9627b6278f6190cd75cd41688a5
mysoyul <vega2k_@naver.com>
2021-12-20 15:53:29 +0900

    dao

---
+++ src/main/java/myspringboot/reactive/r2dbc/dao/CustomerDao.java
@@ -0,0 +1,26 @@
+package myspringboot.reactive.r2dbc.dao;
+
+import myspringboot.reactive.r2dbc.dto.Customer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CustomerDao {
+    private static void sleepExecution(int i){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Customer> getCustomers() {
+        return IntStream.rangeClosed(1, 10)
+                //.peek(i -> sleepExecution(i))
+                .peek(CustomerDao::sleepExecution)
+                .peek(i -> System.out.println("processing count : " + i))
+                .mapToObj(i -> new Customer(i, "Customer" + i))
+                .collect(Collectors.toList());
+    }
+}
